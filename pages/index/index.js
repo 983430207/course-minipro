466e2b7e63d6f3cdc47bd16ca639b57ba4ac6a92
bobi <983430207@qq.com>
2020-03-12 16:08:36 +0800

    07 - 02 - 04 发表留言

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,31 @@ const app = getApp()
 
 Page({
   data: {
-    
+    username:'',
+    content:''
   },
   onLoad: function () {
-    
+
   },
+  saveGbook: function (e) {
+    console.log(e.detail.value)
+    wx.request({
+      url: 'http://laravel1024.com/api/v1/msgs',
+      data: e.detail.value,
+      header: { 'Accept': 'application/json' },
+      method: 'POST',
+      success: (data) => {
+        if(data.statusCode==200){
+          this.setData({
+            username:'',
+            content:''            
+          })
+          wx.showToast({title:'发表成功'})
+        }else{
+          wx.showToast({title:'发表失败',icon:'none'})
+        }
+        console.log(data)
+      }
+    })
+  }
 })
