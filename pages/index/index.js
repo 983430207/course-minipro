71e5c90fcc740414725ead2366ed73cf70e78423
bobi <983430207@qq.com>
2020-03-12 20:28:11 +0800

    07 - 02 - 06 触底翻页

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,12 +4,13 @@ const app = getApp()
 
 Page({
   data: {
-    username:'',
-    content:'',
-    msgs:[],
+    username: '',
+    content: '',
+    msgs: [],
+    height:0,
   },
   //获取留言集合
-  getMsgs:function(){
+  getMsgs: function () {
     wx.request({
       url: 'http://laravel1024.com/api/v1/msgs',
       // data: e.detail.value,
@@ -17,14 +18,54 @@ Page({
       method: 'GET',
       success: (data) => {
         this.setData({
-          msgs:data.data
+          msgs: data.data
         })
         console.log(data.data)
       }
-    })    
+    })
+  },
+  more: function (e) {
+    let current_page = this.data.msgs.meta.current_page
+    let last_page = this.data.msgs.meta.last_page
+    if (current_page < last_page) {
+      wx.request({
+        url: 'http://laravel1024.com/api/v1/msgs',
+        data: {
+          page:current_page+1
+        },
+        header: { 'Accept': 'application/json' },
+        method: 'GET',
+        success: (data) => {
+          data.data.data = this.data.msgs.data.concat( data.data.data )
+          this.setData({
+            msgs: data.data
+          })          
+          console.log("下一页的数据",data.data)
+        }
+      })
+
+    } else {
+      wx.showToast({ title: '无更多数据' })
+    }
+
   },
   onLoad: function () {
     this.getMsgs()
+  },
+  //生命周期函数，页面获得展示时触发
+  onShow: function(){
+    //获取窗口高度
+    let h = wx.getSystemInfoSync().windowHeight 
+    //得到发表区节点对象，并追加 boundingClientRect() 方法，用于获取发表区高度
+    let write = wx.createSelectorQuery().select("#write").boundingClientRect()
+    //执行并获取结果
+    write.exec((res) => {
+      //输出计算结果
+      this.setData({
+        height:h-res[0].height
+      })
+      console.log( h-res[0].height )
+    })
   },
   saveGbook: function (e) {
     console.log(e.detail.value)
@@ -34,15 +75,15 @@ Page({
       header: { 'Accept': 'application/json' },
       method: 'POST',
       success: (data) => {
-        if(data.statusCode==200){
+        if (data.statusCode == 200) {
           this.setData({
-            username:'',
-            content:''            
+            username: '',
+            content: ''
           })
-          wx.showToast({title:'发表成功'})
+          wx.showToast({ title: '发表成功' })
           this.getMsgs()
-        }else{
-          wx.showToast({title:'发表失败',icon:'none'})
+        } else {
+          wx.showToast({ title: '发表失败', icon: 'none' })
         }
         console.log(data)
       }
